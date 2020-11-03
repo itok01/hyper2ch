64be5135cfa1a6f3670c3c08573b5b9ca6f40bb4
itok01 <28337009+itok01@users.noreply.github.com>
2020-11-03 21:46:54 +0900

    Fix

--- src/services/legacy/bbs.rs
+++ src/services/legacy/bbs.rs
@@ -1,6 +1,6 @@
 use crate::diesel::prelude::*;
 use crate::models_diesel::*;
-use crate::schema::bbs::dsl::*;
+use crate::schema::bbses::dsl::*;
 use crate::util::convert_to_shift_jis;
 use actix_web::{get, web, HttpResponse, Responder};
 use bytes::Bytes;
@@ -11,7 +11,7 @@ use htmlescape::encode_minimal;
 pub async fn get_bbs_handler(web::Path(bbs_path_name): web::Path<String>) -> impl Responder {
     let conn = establish_connection();
 
-    let result = bbs
+    let result = bbses
         .filter(path_name.eq(bbs_path_name))
         .first::<Bbs>(&conn)
         .expect("Error");
@@ -28,7 +28,7 @@ pub async fn get_bbs_handler(web::Path(bbs_path_name): web::Path<String>) -> imp
 pub async fn get_bbs_menu_handler() -> impl Responder {
     let conn = establish_connection();
 
-    let result = bbs
+    let result = bbses
         .filter(hidden.eq(false))
         .load::<Bbs>(&conn)
         .expect("Error");
@@ -45,21 +45,21 @@ pub async fn get_bbs_menu_handler() -> impl Responder {
 
 /// Generate bbsmenu.txt for listing BBSes
 /// TODO: Fix problem that 2ch browser cannot load
-fn generate_bbsmenu_html(bbses: Vec<Bbs>) -> Option<Bytes> {
-    if bbses.len() == 0 {
+fn generate_bbsmenu_html(bbs_vec: Vec<Bbs>) -> Option<Bytes> {
+    if bbs_vec.len() == 0 {
         return None;
     }
 
     let backend_address = dotenv!("BACKEND_ADDRESS");
 
     let mut bbs_list = String::from(BBSMENU_HTML_TOP);
-    let mut cat = bbses[0].category.clone();
+    let mut cat = bbs_vec[0].category.clone();
     bbs_list.push_str(&format!(
         "<h1><a href={}>hyper2ch</a></h1>\n<h2>BBS MENU</h2>\n<ul>\n",
         backend_address
     ));
     bbs_list.push_str(&format!("<li><h3>{}</h3></li>\n<ul>\n", cat));
-    for b in bbses {
+    for b in bbs_vec {
         if cat != b.category {
             cat = b.category;
             bbs_list.push_str(&format!("</ul>\n<li><h3>{}</h3></li>\n<ul>\n", cat));
